e000070fd4e7c61417c21e6617785cba5e26eb65
Erik Michaels-Ober <erik@instacart.com>
2016-02-05 17:31:05 -0800

    Set default batch size with ability to override

--- lib/active_record/import_with_callbacks.rb
+++ lib/active_record/import_with_callbacks.rb
@@ -4,9 +4,43 @@ require 'fiber'
 
 module ActiveRecord
   module ImportWithCallbacks
+    Result = Class.new(ActiveRecord::Import::Result)
+    DEFAULT_BATCH_SIZE = 100
     extend ActiveSupport::Concern
+
+    # Insert multiple records in batches with callbacks
+    #
+    # @param [Array<ActiveRecord::Base>] records
+    # @param [Hash] options
+    # @option options [Boolean] :validate Default: true
+    # @option options [Array, Hash] :on_duplicate_key_update
+    # @option options [Array<ActiveRecord::Base>] :synchronize Synchronize
+    #   existing records in memory with updates from the import
+    # @option options [Boolan] :timestamps
+    # @option options [Boolan] :recursive Import all autosave associations
+    # @option options [Integer] :batch_size Default: 100
+    # @return [ActiveRecord::Import::Result]
     def import_with_callbacks(*args)
-      import(*args)
+      options = args.extract_options!
+      records = args.pop
+      options[:batch_size] = DEFAULT_BATCH_SIZE unless options.key?(:batch_size)
+      import_in_batches(records, options)
+    end
+
+    private
+
+    def import_in_batches(records, options)
+      results = records.each_slice(options.fetch(:batch_size)).map do |slice|
+        import(slice, options)
+      end
+      merge_results(results)
+    end
+
+    def merge_results(results)
+      failed_instances = results.inject([]) { |a, e| a + e.failed_instances }
+      num_inserts = results.inject(0) { |a, e| a + e.num_inserts }
+      ids = results.inject([]) { |a, e| a + e.ids }
+      Result.new(failed_instances, num_inserts, ids)
     end
   end
 
--- spec/active_record/import_with_callbacks_spec.rb
+++ spec/active_record/import_with_callbacks_spec.rb
@@ -24,6 +24,21 @@ describe ActiveRecord::ImportWithCallbacks do
     brands = Array.new(2) { |i| Brand.new(name: "Brand #{i}") }
     result = Brand.import_with_callbacks(brands)
     expect(result.num_inserts).to eq(1)
+    expect(result.ids.size).to eq(2)
+  end
+
+  it 'batches imports into groups of 100 (by default)' do
+    brands = Array.new(101) { |i| Brand.new(name: "Brand #{i}") }
+    result = Brand.import_with_callbacks(brands)
+    expect(result.num_inserts).to eq(2)
+    expect(result.ids.size).to eq(101)
+  end
+
+  it 'allows the user to override the default batch size' do
+    brands = Array.new(11) { |i| Brand.new(name: "Brand #{i}") }
+    result = Brand.import_with_callbacks(brands, batch_size: 5)
+    expect(result.num_inserts).to eq(3)
+    expect(result.ids.size).to eq(11)
   end
 
   it 'returns ids for successfully imported records' do
